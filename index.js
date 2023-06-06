aa234a9772cf4ba68b51a28c4ecf6f97c77411a4
Saransh <saranshgautam07@gmail.com>
2023-06-06 22:34:20 +0530

    firstcommit

---
+++ index.js
@@ -0,0 +1,10 @@
+var number1=56;
+var number2=89;
+console.log(number1+number2);
+var age=prompt("Enter your age:")
+if(age>18){
+    console.log("You can drive")
+}
+else{
+    console.log("go ride your cycle")
+}
